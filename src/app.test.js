861894db6e43e1662acfe075b3c195ab3d235369
attedib <45427411+attedib@users.noreply.github.com>
2020-04-08 00:38:23 +0200

    test 2

--- src/app.test.js
+++ src/app.test.js
@@ -2,6 +2,6 @@ const { dayOfTheWeek } = require('./app');
 
 
 test('getDay returner et langt format af dagen af ugen', () => {
-    const day = dayOfTheWeek( new Date('4/8/2020') );
+    const day = dayOfTheWeek( new Date('4/9/2020') );
     expect( day ).toBe('Onsdag');
 });
